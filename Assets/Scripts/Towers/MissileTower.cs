7891bf8ac6ff6b8cdade946309429e6ae1117f79
jelmersnippe <jelmersnippe@hotmail.com>
2022-02-13 23:18:44 +0100

    Add missile tower upgrades

--- Assets/Scripts/Towers/MissileTower.cs
+++ Assets/Scripts/Towers/MissileTower.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class MissileTower : Tower
 {
-    [SerializeField] float splashRadius;
-    [SerializeField] int splashDamage;
-    [SerializeField] int maxSplashTargetCount;
+    [SerializeField] float splashRadius = 3f;
+    [SerializeField] int maxSplashTargetCount = 4;
 
     override protected void SpawnProjectile()
     {
         SplashProjectile spawnedProjectile = Instantiate(projectile, firePoint.position, firePoint.rotation, transform) as SplashProjectile;
-        spawnedProjectile.setValues(damage, projectileSpeed, currentTarget.transform, monsterLayerMask, splashRadius, splashDamage, maxSplashTargetCount);
+        spawnedProjectile.setValues(damage, projectileSpeed, currentTarget.transform, monsterLayerMask, splashRadius, damage, maxSplashTargetCount);
+    }
+
+    override public void ActivateUpgrade(Upgrade upgradeToActivate)
+    {
+        base.ActivateUpgrade(upgradeToActivate);
+
+        switch (upgradeToActivate.type)
+        {
+            case "missile_damage_up":
+                damage += 50;
+                break;
+            case "missile_splash_radius_up":
+                splashRadius += 1;
+                break;
+            case "missile_target_count_up":
+                maxSplashTargetCount += 2;
+                break;
+        }
     }
 }
